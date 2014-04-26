688bc7621e3f9cf3338fb84f1b6d59386837c18e
petertbernhardt <petertbernhardt@gmail.com>
2014-04-26 16:44:03 -0700

    Changed Buttons to ToggleButtons. Edited strings.xml to have values for both states of each ToggleButton. Changed event handler to work for the ToggleButtons, added pseudocode for turning stuff back on, and added code to turn off the sound (works!).

--- src/com/peter/hitthelights/MainPage.java
+++ src/com/peter/hitthelights/MainPage.java
@@ -3,6 +3,7 @@ package com.peter.hitthelights;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.media.AudioManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
@@ -10,20 +11,23 @@ import android.content.Context;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainPage extends Activity implements OnClickListener {
+public class MainPage extends Activity implements OnCheckedChangeListener {
 
 	// Fields
-	private Button master, sound;
-	private Map<String, Boolean> settings;
+	private ToggleButton master, sound;
+	private Map<String, String> settings;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_page);
 		setUpButtons();
-		settings = new HashMap<String, Boolean>();
+		settings = new HashMap<String, String>();
 	}
 
 	@Override
@@ -36,43 +40,55 @@ public class MainPage extends Activity implements OnClickListener {
 	// Private helper method for onCreate that handles setting up
 	// the buttons.
 	private void setUpButtons() {
-		master = (Button) findViewById(R.id.master);
-		sound = (Button) findViewById(R.id.sound);
-		master.setOnClickListener(this);
-		sound.setOnClickListener(this);
+		master = (ToggleButton) findViewById(R.id.master);
+		sound = (ToggleButton) findViewById(R.id.sound);
+		master.setOnCheckedChangeListener(this);
+		sound.setOnCheckedChangeListener(this);
 	}
 
 	@Override
-	public void onClick(View button) {
+	public void onCheckedChanged(CompoundButton button, boolean state) {
 		if (button.equals(master)) {
-			// Before turning off settings, remember what the user had turned on and off,
-			// so when turning stuff back on, the app doesn't turn on things that
-			// weren't originally on. EX: setting bluetooth on when it was originally off.
-			/* TURN OFF:
-			*	WIFI
-			*	GPS
-			*	SOUND
-			*	BLUETOOTH
-			*  SET BRIGHTNESS TO 0
-			*/
-			// Hold current settings in Map<String, Boolean>
-			// Wifi
-			WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
-			if (wifiManager.isWifiEnabled()) {
-				// put in settings
-				settings.put("wifi", true);
-				// turn off wifi
-				wifiManager.setWifiEnabled(false);
+			if (state) {
+				// Before turning off settings, remember what the user had turned on and off,
+				// so when turning stuff back on, the app doesn't turn on things that
+				// weren't originally on. EX: setting bluetooth on when it was originally off.
+				/* TURN OFF:
+				*	WIFI - done
+				*	GPS - ?
+				*	SOUND - done
+				*	BLUETOOTH
+				*  SET BRIGHTNESS TO 0
+				*/
+				// Hold current settings in Map<String, Boolean>
+				// Wifi
+				WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+				if (wifiManager.isWifiEnabled()) {
+					// put in settings
+					settings.put("wifi", "on");
+					// turn off wifi
+					wifiManager.setWifiEnabled(false);
+				}
+				// Sound
+				AudioManager aManager=(AudioManager)getSystemService(AUDIO_SERVICE);
+				settings.put("sound", Integer.valueOf(aManager.getRingerMode()).toString());
+				aManager.setRingerMode(aManager.RINGER_MODE_SILENT);
+			} else {
+				// go through the settings map and turn everything that was on back on
+				Toast.makeText(getApplicationContext(), "blahhhhh", Toast.LENGTH_LONG).show();
+				for (String setting : settings.keySet()) {
+					
+				}
 			}
-			
-			// After changing settings, change button text to say:
-			// Turn on Everything!
 		} else if (button.equals(sound)) {
-			// Remember previous setting so when resetting the volume, go back to the previous
-			// level
-			// TURN OFF SOUND
-			// After changing settings, change button text to say:
-			// Yes sound!
+			if (state) {
+				// Remember previous setting so when resetting the volume, go back to the previous
+				// level
+				// TURN OFF SOUND
+			} else {
+				// turn the sound back on
+				
+			}
 		}
 	}
 
